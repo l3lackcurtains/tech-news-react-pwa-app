ae9f4f2f1761b05a44fca49c8ace9900bd59e42b
Madhav Poudel <king.ov.universe@gmail.com>
2017-01-07 08:57:30 +0545

    Prod changes

--- server/server.js
+++ server/server.js
@@ -4,11 +4,7 @@ const express = require('express')
 module.exports = {
   app: function () {
     const app = express()
-    if (process.env.NODE_ENV == 'production') {
-    	const indexPath = path.join(__dirname, '/../build/index.html')
-    }else{
-    	const indexPath = path.join(__dirname, '/../public/index.html')
-    }
+    const indexPath = path.join(__dirname, '/../public/index.html')
     const publicPath = express.static(path.join(__dirname, '../build'))
 
     app.use('/build', publicPath)
