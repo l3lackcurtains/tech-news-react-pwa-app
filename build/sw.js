a6a5f44dcb7ce9b44a0da1e9f078222611f854d7
sytechno <l3lackcurtains@github.com>
2019-02-10 16:51:17 +0545

    heroku configuration

---
+++ build/sw.js
@@ -0,0 +1,29 @@
+importScripts("/precache-manifest.c5dd4a51d611fe5c3acd731070a25eba.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+const staticFiles = [
+  "./manifest.json",
+  "./favicon.png",
+  "./icons/app-icon-256.png",
+  "./icons/app-icon.png",
+  "./icons/app-icon.svg"
+];
+
+workbox.clientsClaim();
+self.__precacheManifest = staticFiles.concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/index.html", {
+  blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/]
+});
+
+workbox.routing.registerRoute(
+  new RegExp("https://newsapi.org/(.*)"),
+  workbox.strategies.networkFirst()
+);
+
