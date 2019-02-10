e17493140dfbeef72492e4f322059e97c21e2ee0
sytechno <l3lackcurtains@github.com>
2019-02-10 17:02:22 +0545

    heroku configuration

---
+++ build/precache-manifest.92274315ea86ebf31d8827527e40c849.js
@@ -0,0 +1,37 @@
+self.__precacheManifest = [
+  {
+    "revision": "d141147887f3e5a5167260bd24ce43c9",
+    "url": "/static/media/long-light-logo.d1411478.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "160d5ce71f7c79acb52b",
+    "url": "/static/js/main.160d5ce7.chunk.js"
+  },
+  {
+    "revision": "30b24ca342c4ad082c78",
+    "url": "/static/js/1.30b24ca3.chunk.js"
+  },
+  {
+    "revision": "160d5ce71f7c79acb52b",
+    "url": "/static/css/main.97d59e2d.chunk.css"
+  },
+  {
+    "revision": "30b24ca342c4ad082c78",
+    "url": "/static/css/1.b96ad11d.chunk.css"
+  },
+  {
+    "revision": "1a47e7fa54fe5282eee665482e60fab4",
+    "url": "/service-worker.js"
+  },
+  {
+    "url": "/precache-manifest.c78d33824b686d35f66a06cf437b60c1.js"
+  },
+  {
+    "revision": "b0a61e5e797eef95eaa8a2419310e451",
+    "url": "/index.html"
+  }
+];
